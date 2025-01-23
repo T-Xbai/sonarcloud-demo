25043c2f2c1a3ced36ef0688457a7ddfd8589775
WXH <wangxiaohao@hungrypanda.onaliyun.com>
2025-01-23 15:30:52 +0800

    first commit

---
+++ src/test/java/com/example/sonarclouddemo/SonarcloudDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.sonarclouddemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SonarcloudDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
