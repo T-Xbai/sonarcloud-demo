5c44fcb2068813795a699768e04e254ee2a8d72b
WXH <wangxiaohao@hungrypanda.onaliyun.com>
2025-01-24 10:59:51 +0800

    4 commit

--- src/main/java/com/example/sonarclouddemo/SonarcloudDemoApplication.java
+++ src/main/java/com/example/sonarclouddemo/SonarcloudDemoApplication.java
@@ -2,9 +2,27 @@ package com.example.sonarclouddemo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RequestMapping
+@RestController
 public class SonarcloudDemoApplication {
+
+    @GetMapping("/api")
+    public boolean demo(@RequestParam Boolean res){
+
+        if (res){
+            return true;
+        }
+        if (res){
+            return false;
+        }
+        return true;
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(SonarcloudDemoApplication.class, args);
